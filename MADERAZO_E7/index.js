b6654838d23f5061e888f00ad454e316c99a9d84
Rafael Maderazo <rafaelmaderazo@gmail.com>
2020-03-11 20:29:59 +0800

    Removed Academics

--- MADERAZO_E7/index.js
+++ MADERAZO_E7/index.js
@@ -19,114 +19,7 @@ var person = {
 
 var acads = 
 {
-       "first" : 
-       [
-              {
-                     "course": "CCPROG1",
-                     "prof": "Ms. Shirley Chu"
-              },
-              {
-                     "course": "BASMATH",
-                     "prof": "Mr. Melvin Vidar"
-              },
-              {
-                     "course": "BASSTAT",
-                     "prof": "Mr. Karl Collado"
-              },
-              {
-                     "course": "CCICOMP",
-                     "prof": "Mr. Roger Uy"
-              },
-              {
-                     "course": "GEPCOMM",
-                     "prof": "Ms. Gene Flores"
-              },
-              {
-                     "course": "CCPROG2",
-                     "prof": "Mr. Arturo Caronongan"
-              },
-              {
-                     "course": "CCDSTRU",
-                     "prof": "Mr. Pauline Rivera"
-              },
-              {
-                     "course": "CSMATH1",
-                     "prof": "Mr. Edmundo Perez"
-              },
-              {
-                     "course": "GEUSELF",
-                     "prof": "Mr. Martin Esteves"
-              },
-              {
-                     "course": "GEARTAP",
-                     "prof": "Ms. Maysa Arabit"
-              },
-              {
-                     "course": "GEMATMW",
-                     "prof": "Ms. Diane Songsong"
-              },
-              {
-                     "course": "CCPROG3",
-                     "prof": "Ms. Nathalie Lim-Cheng"
-              },
-              {
-                     "course": "CCDSALG",
-                     "prof": "Ms. Teresita Limoanco"
-              },
-              {
-                     "course": "GEFILI1",
-                     "prof": "Ms. Myrna Torreliza"
-              },
-              {
-                     "course": "GEFTWEL",
-                     "prof": "Mr. Aguinaldo"
-              }
-
-       ],
-       "second" : 
-       [
-              {
-                     "course": "CSADPRG",
-                     "prof": "Mr. ?"
-              },
-              {
-                     "course": "CCINFOM",
-                     "prof": "Mr. Oliver Malabanan"
-              },
-              {
-                     "course": "CSNETWK",
-                     "prof": "Mr. Gregory Cu"
-              },
-              {
-                     "course": "GEFILI2",
-                     "prof": "Mr. ?"
-              },
-              {
-                     "course": "GEDANCE",
-                     "prof": "Ms. Joanna"
-              },
-              {
-                     "course": "CSALGCM",
-                     "prof": "Ms. Austin Fernandez"
-              },
-              {
-                     "course": "CCAPDEV",
-                     "prof": "Mr. Arren Antioquia"
-              },
-              {
-                     "course": "NSCOM01",
-                     "prof": "Ms. Arlyn Ong"
-              },
-              {
-                     "course": "NSSECU1",
-                     "prof": "Mr. Katrina Solomon"
-              },
-              {
-                     "course": "CSARCH1",
-                     "prof": "Mr. Ronald Pascual"
-              }
-
-       ],
+       
        "activities" : 
        [
               {
@@ -169,10 +62,6 @@ app.get("/profile", function(req, res)
        res.render("about", person["studs"]);
 });
 
-app.get("/academics", function(req, res)
-{
-       res.render("academics", acads);
-});
 
 app.get("/activities", function(req, res)
 {
